4e25c895a06a320a20a259b0f99c61c8f6d13dc7
almaz <almazburan@gmail.com>
2020-03-07 17:41:57 -0500

    This is my FirstCommit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAPAndroid() {
+		System.out.println("NativeAPAndroid");
+	}
+	
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApss");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	//testing xml file from Maven
+	//how to execute all test cases from Test folder -mvn test
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	
+	
+	@Test
+	public void elementsUi() {
+		System.out.println("elementUi");
+	}
+
+}
